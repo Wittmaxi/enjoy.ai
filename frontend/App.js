ffe161e6723e51a9d5bc8e14f15a80d3873ece6b
Tobias Jern <tobias.jern@aalto.fi>
2022-11-04 21:58:33 +0200

    Add component library and buttons

--- frontend/App.js
+++ frontend/App.js
@@ -1,11 +1,21 @@
+import React, {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
+import { Button, ButtonGroup }  from '@rneui/themed';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [selectedIndex, setSelectedIndex] = useState(3);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text>Open up App.js to start working on your app!</Text>
+    <ButtonGroup
+      buttons={['Mad', 'Sad', 'Glad', 'Depressed']}
+      selectedIndex={selectedIndex}
+      onPress={(value) => {
+        setSelectedIndex(value);
+      }}
+      containerStyle={{ marginBottom: 20 }}
+    /><StatusBar style="auto" />
     </View>
   );
 }
