9575fe6c86a04234dd26eef118c3be6144b4f442
Maximilian Wittmer <wittmaxi@linux.pizza>
2022-11-05 16:44:08 +0200

    [IMP:merge_audio.py] create audio files by merging file sources and looping them to the required lengthw

---
+++ frontend/components/MoodPicker.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { Icon } from "@rneui/base";
+
+export default class MoodPicker extends React.Component {
+    render () {
+        
+    }
+}
