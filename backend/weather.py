e47ba73979887dd89c7f7ff3c072292847885de5
Peik Etzell <peik.etzell@gmail.com>
2022-11-05 02:23:28 +0200

    now returns dict

--- backend/weather.py
+++ backend/weather.py
@@ -8,7 +8,53 @@ def get_temperature(location):
                             args=['bbox=24,59,26,60.5',
                             'timeseries=True'])
     data = observations.data[location]
-    return data['t2m']['values'][-1] # temperature, last observation
+    print(data.keys())
+
+    # times timestamp
+    # t2m temperature
+    # ws_10min wind speed
+    # wg_10min
+    # rh rain
+    # td dew point?
+    # r_1h rain 1h
+    # ri_10min rain instant
+    # snow_aws snow amount
+    # p_sea pressure
+    # vis meters
+
+    temp = data['t2m']['values'][-1]
+    wind_speed = data['ws_10min']['values'][-1]
+    rain = data['ri_10min']['values'][-1]
+
+
+    # if rain == 0.0:
+    #     if temp < 5 or wind_speed > 10:
+    #         return 'cold'
+    #     elif wind_speed > 15:
+    #         return 'intense'
+    #     elif temp > 17:
+    #         return 'pleasant'
+    #     else:
+    #         return ''
+    #        
+    #        
+    #
+    #    
+    # else:
+    #     if temp < 10:
+    #         if wind_speed > 10:
+    #             return 'intense'
+    #         else:
+    #             return 'gloomy'
+    #     else:
+    #         if wind_speed > 10:
+    #             return 
+        
+
+
+
+    # data['t2m']['values'][-1] # temperature, last observation
+    return {'temperature': temp, 'wind': wind_speed, 'rain': rain}
 
 if __name__=='__main__':
     print(get_temperature('Espoo Tapiola'))
