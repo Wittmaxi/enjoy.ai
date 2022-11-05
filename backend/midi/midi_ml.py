1fc9ef37dbaf341594630c9162e71e49b24dd9aa
Peik Etzell <peik.etzell@gmail.com>
2022-11-05 16:16:50 +0200

    asdf

--- backend/midi/midi_ml.py
+++ backend/midi/midi_ml.py
@@ -1,49 +1,79 @@
 from __future__ import division
 import random
 import sys
-from mido import Message, MidiFile, MidiTrack, MAX_PITCHWHEEL
+from mido import Message, MetaMessage, MidiFile, MidiTrack, MAX_PITCHWHEEL, bpm2tempo
 import fluidsynth
 from midi2audio import FluidSynth
 
-def fs_create_ai():
-    pass
-    
-# create random midi, save file as 
-def ai_create_midi(filename) -> None:
-    notes = [x for x in range(50, 100)]
 
-    outfile = MidiFile()
+class AI_synth():
+    def __init__(self) -> None:
+        self.bpm = 120
+        self.mean = 70
+        self.bpm_target = 70
+        self.mean_target = 120
+        pass
 
-    track = MidiTrack()
-    outfile.tracks.append(track)
+    def update_bpm(self, bpm, force=False):
+        self.bpm_target = bpm
+        if force:
+            self.bpm = bpm
 
-    track.append(Message('program_change', program=12))
+    def update_tone(self, tone, force=False):
+        self.mean_target = tone
+        if force:
+            self.mean = tone
 
-    delta = 300
-    ticks_per_expr = int(sys.argv[1]) if len(sys.argv) > 1 else 20
-    for i in range(100):
-        # note = random.choice(notes)
-        note = notes[i % len(notes)]
-        vel = random.randint(10, 127)
-        track.append(Message('note_on', note=note, velocity=vel, time=delta))
-        for j in range(delta // ticks_per_expr):
-            pitch = MAX_PITCHWHEEL * j * ticks_per_expr // delta
-            track.append(Message('pitchwheel', pitch=pitch, time=ticks_per_expr))
-        track.append(Message('note_off', note=note, velocity=vel, time=0))
-    outfile.save(filename)
-    return 
+    # create random midi, save file as 
+    def ai_create_midi(self, duration: float, filename) -> None:
+        notes = [x for x in range(50, 100)]
 
-def midi_to_wav(file, output_filename):
-    fs = FluidSynth('/usr/share/soundfonts/FluidR3_GM.sf2')
-    fs.midi_to_audio(file, output_filename)
+        outfile = MidiFile()
 
-def ai_create_wav(output_filename):
-    tmp_name = 'tmp.mid'
-    ai_create_midi(tmp_name)
-    midi_to_wav(tmp_name, output_filename)
+        track = MidiTrack()
+        outfile.tracks.append(track)
+
+        track.append(Message('program_change', program=12))
+
+        delta = 300
+        ticks_per_expr = int(sys.argv[1]) if len(sys.argv) > 1 else 20
+
+        elapsed = 0
+        while elapsed < duration:
+            track.append(MetaMessage('set_tempo', tempo=bpm2tempo(self.bpm)))
+
+            note = int(min(127, max(0, random.gauss(self.mean, 20))))
+            vel = int(random.gauss(64, 10))
+            track.append(Message('note_on', note=note, velocity=vel, time=delta))
+            for j in range(delta // ticks_per_expr):
+                pitch = MAX_PITCHWHEEL * j * ticks_per_expr // delta
+                track.append(Message('pitchwheel', pitch=pitch, time=ticks_per_expr))
+            track.append(Message('note_off', note=note, velocity=vel, time=0))
+
+            # change bpm towards target
+            bpm_diff = self.bpm_target - self.bpm
+            self.bpm += min(1.1, max(-1.1, bpm_diff))
+            mean_diff = self.mean_target - self.mean
+            self.mean += min(0.5, max(-0.5, mean_diff))
+            elapsed = outfile.length
+        outfile.save(filename)
+        return 
+
+    def midi_to_wav(self, file, output_filename):
+        fs = FluidSynth('/usr/share/soundfonts/FluidR3_GM.sf2')
+        fs.midi_to_audio(file, output_filename)
+
+    def ai_create_wav(self, duration, output_filename):
+        tmp_name = 'tmp.mid'
+        self.ai_create_midi(duration, tmp_name)
+        self.midi_to_wav(tmp_name, output_filename)
     
 if __name__=='__main__':
-    midi_file = 'ai_output.mid'
+    synth = AI_synth()
+    synth.update_bpm(150, force=True)
+    synth.update_bpm(100)
+    synth.update_tone(30, force=True)
+    synth.update_tone(100)
+    # midi_file = 'ai_output.mid'
     wav_file = 'ai_output.wav'
-    ai_create_midi(midi_file)
-    midi_to_wav(midi_file, wav_file)
+    synth.ai_create_wav(60, wav_file)
