e965f84b5ef461a08c4ad2ece3f136c6a2848fc6
LinuzJ <linusjern00@gmail.com>
2022-11-07 09:52:03 +0200

    mod safe_install comments

--- backend/safe_install.sh
+++ backend/safe_install.sh
@@ -1,4 +1,4 @@
-# safe_install.sh installs the FluidR3_GM.sf2 onto the container environment in a safe manner
+# safe_install.sh downloads the FluidR3_GM.sf2 into the container environment in a safe manner
 
 wget https://keymusician01.s3.amazonaws.com/FluidR3_GM.zip
 
